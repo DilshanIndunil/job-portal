841f8595c9f58fa95ef08a397caf854c8ec23855
DilshanIndunil <dilshanindunilrajapaksha1@gmail.com>
2025-08-31 12:10:47 +0530

    Add backend code

---
+++ backend/controllers/company.controller.js
@@ -0,0 +1,91 @@
+import { Company } from "../models/company.model";
+
+export const registerCompany = async (req , res) => {
+    try {
+        const {companyName} = req.body;
+    if(!companyName){
+        return res.status(400).json({message:"Company name required", success:false});
+    }
+
+    let company = await Company.findOne({name:companyName}) ;
+    if(company){
+        return res.status(400).json({message:"You can't register same company"});
+    }
+
+    company = await Company.create({
+        name:companyName,
+        userID: req.id
+    })
+
+    return res.status(200).json({
+        message:"Company registered successfully",
+        company,
+        success: true
+    })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Server error", success: false });
+    }
+}
+
+export const getCompany = async (req, res) => {
+    try {
+        const userId = req.id;
+        const companies = await Company.find({userId});
+    
+        if(!companies){
+            return res.status(404).json({message: "Companies not found", success:false});
+        }
+
+        return res.status(200).json({companies, success:true});
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Server error", success: false });
+    }
+}
+
+export const getCompanyById = async (req, res) => {
+    
+    try {
+        const companyId = req.params.id;
+        const company = await Company.findById(companyId);
+    
+        if(!company) {
+            return res.status(404).json({message: "Company not found", success:false});
+        }
+    
+        return res.status(200).json({company, success:true});
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Server error", success: false });
+    }
+
+
+}
+
+export const updateCompny = async (req, res) => {
+    
+    try {
+        const {name, description, website, location} = req.body;
+        const file = req.file;
+        // cloudinary implementation
+    
+        const updateDate = {name, description, website, location};
+    
+        const company = await Company.findByIdAndUpdate(req.params.id, updateDate, {new:true});
+        if(!comapny){
+            return res.status(404).json({message: "Company not found !", success:false})
+        }
+
+        return res.status(200).json({
+            message: "Company details updated !",
+            company,
+            success:true
+        })
+        
+    } catch (error) {
+        return res.status(500).json({message: "Internal server Error", success:false});
+    }
+}
---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js'
+import userRoute from './routes/user.route.js';
+
+const app = express();
+dotenv.config();
+
+app.get('/home', (req, res) => {
+    return res.status(200).json({
+        message: "I am comming from backend",
+        success: true
+    })
+})
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser());
+
+// routes 
+app.use("/api/v1/user", userRoute);
+
+// CORS configuration
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+}
+
+app.use(cors(corsOptions));
+
+app.listen(process.env.PORT || 3000 , () => {
+    connectDB();
+    console.log(`Server is running on http://localhost:${process.env.PORT || 3000}`);
+})
